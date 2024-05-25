7d9d597d70e316ac4c7da5d1dc40e6ae8488ec70
idermailer <idermailer@gmail.com>
2024-05-25 09:08:37 +0900

    초기 커밋.

---
+++ DresserSorter/ModConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace DresserSorter
+{
+    public sealed class ModConfig
+    {
+        public bool VeryTidySort_ByStats { get; set; } = false;
+        public bool SortAdditionalCategories { get; set; } = false;
+        public bool Cloth_ColorSortByHue { get; set; } = true;
+        public bool Ring_CombinedRingIsPrior { get; set; } = true;
+        public bool Object_NotSortResourceByDisplayName { get; set; } = true;
+        public bool Furniture_NotCompareFurnitureType { get; set; } = true;
+    }
+}
---
+++ DresserSorter/ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley.Objects;
+
+namespace DresserSorter
+{
+    internal sealed class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            SortingMethods.config = Helper.ReadConfig<ModConfig>();
+            SortingMethods.monitor = Monitor;
+            SortingMethods.modHelper = helper;
+            var harmony = new Harmony(ModManifest.UniqueID);
+
+            // this mod only patch one method for the sorting logic
+            harmony.Patch(
+                AccessTools.Method(typeof(StorageFurniture), nameof(StorageFurniture.SortItems)),
+                prefix: new HarmonyMethod(typeof(SortingMethods), nameof(SortingMethods.SortOverride))
+            );
+
+            //Helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
+        }
+
+
+        /*
+        private void GameLoop_GameLaunched(object? sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
+        {
+            // Support for Generic Mod Connfig Menu may be added in the future. or not.
+        }
+        */
+    }
+}
